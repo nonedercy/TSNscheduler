9bdb843f56904e0107067c52e8d1034a205324f1
nonedercy <ayyzlidaxin@163.com>
2019-06-17 16:11:45 +0800

    输入流数据

--- cyber.py
+++ cyber.py
@@ -11,12 +11,20 @@ from gurobipy import tupledict
 from gurobipy import multidict
 from gurobipy import Model
 from gurobipy import quicksum
+import matplotlib.pyplot as plt     
 import os
+import networkx as nx
 import xlrd
+def draw(G): 
+    plt.rcParams['figure.dpi'] = 100                          
+    pos=nx.spring_layout(G)
+    nx.draw(G,pos,arrows=True,with_labels=True,nodelist=G.nodes(),style='dashed',
+            edge_color='b',width=2,node_color='y',alpha=0.5)
+    #保存绘制图像
+    plt.savefig("result.png")
+    plt.show()   
     
 def viewe(path):
-    import networkx as nx
-    import matplotlib.pyplot as plt
     G = nx.Graph()
     book = xlrd.open_workbook(os.path.join(path))
     sh = book.sheet_by_name("E")
@@ -27,10 +35,7 @@ def viewe(path):
             i= i + 1
         except IndexError:
             break
-    nx.draw(G,with_labels=True,font_weight='bold')
-    #保存绘制图像
-    #plt.savefig("wuxiangtu.png")
-    plt.show()
+    draw(G)   
 
 def lcm(x, y):
    if x > y:
@@ -61,7 +66,6 @@ def printSolution():
         print('No solution')
         
 def painte():
-    import matplotlib.pyplot as plt
     from matplotlib import colors
     colorindex = list(colors.CSS4_COLORS.keys())
     lcmT = 1
@@ -72,60 +76,88 @@ def painte():
     for va,vb in E:
         fs = oldf.select(va,vb,'*','*')
         for m in fs:
+            markm = int(m[2]) % len(colorindex)
             for α in range(round(lcmT/oldT[m])):
                 rect = plt.Rectangle((oldφ[m] + α*oldT[m],i-0.5),oldL[m],1,
-                                     color = colorindex[int(m[2][1:])] )
+                                     color = colorindex[markm])
                 ax.add_patch(rect)
                 ax.annotate(m[2]+'.'+str(round(oldρ[m[0],m[1],m[2]])), 
                             (oldφ[m] + α*oldT[m] + oldL[m]/2, i), color='black', 
                             weight='bold', fontsize=12, ha='center', va='center')
         i += 1 
-    plt.xlim((0, lcmT))     #也可写成plt.xlim(-5, 5) 
+    plt.xlim((0, lcmT))    
     plt.ylim((-0.5, len(E)-0.5)) 
     plt.yticks(range(0, len(E)), E)
     plt.show()  
 
-tur = 0
-
-batch = 10
+batch = 100
 δ = 2.5 
-         
-book = xlrd.open_workbook(os.path.join('cyber.xlsx'))
-viewe("cyber.xlsx")
-#边集，存储所有双向边
-sh = book.sheet_by_name("E")
-E = tuplelist([])
-i = 1
-while True:
-    try:
-        E.append((sh.cell_value(i, 0),sh.cell_value(i, 1)))
-        E.append((sh.cell_value(i, 1),sh.cell_value(i, 0)))
-        i= i + 1
-    except IndexError:
-        break  
+source = "txt"
 
-#流集，存储流与流属性        
-sh = book.sheet_by_name("S")
-fullS = {}
-i = 1
-while True:
-    try:    
-        fullS[sh.cell_value(i, 0)] = [sh.cell_value(i, 1),sh.cell_value(i, 2)]
-        i = i + 1
-    except IndexError:
-        break
-       
-#有序存储流实例
-sh = book.sheet_by_name("F")
-fullF = tuplelist([])
-i = 1
-while True:
-    try:
-        fullF.append((sh.cell_value(i, 1),sh.cell_value(i, 2),sh.cell_value(i, 0)))
-        i = i + 1
-    except IndexError:
-        break 
+tur = 0
+fpath = os.path.abspath('..')
+
+if source == "xlsx":         
+    book = xlrd.open_workbook(os.path.join('cyber.xlsx'))
+    viewe("cyber.xlsx")
+    #边集，存储所有双向边
+    sh = book.sheet_by_name("E")
+    E = tuplelist([])
+    i = 1
+    while True:
+        try:
+            E.append((str(int(sh.cell_value(i, 0))),str(int(sh.cell_value(i, 1)))))
+            E.append((str(int(sh.cell_value(i, 1))),str(int(sh.cell_value(i, 0)))))
+            i= i + 1
+        except IndexError:
+            break  
     
+    #流集，存储流与流属性        
+    sh = book.sheet_by_name("S")
+    fullS = {}
+    i = 1
+    while True:
+        try:    
+            fullS[str(int(sh.cell_value(i, 0)))] = [sh.cell_value(i, 1),sh.cell_value(i, 2)]
+            i = i + 1
+        except IndexError:
+            break
+           
+    #有序存储流实例
+    sh = book.sheet_by_name("F")
+    fullF = tuplelist([])
+    i = 1
+    while True:
+        try:
+            fullF.append((str(int(sh.cell_value(i, 1))),str(int(sh.cell_value(i, 2))),str(int(sh.cell_value(i, 0)))))
+            i = i + 1
+        except IndexError:
+            break 
+elif source == "txt":
+    with open(fpath+"\\grid\\grid1",'r') as mnet:
+        grid = mnet.readlines()
+        numofnode = int(grid[0])
+        numofarc = int(grid[1])
+        numoftask = int(grid[2]) 
+        
+        G=nx.Graph()    
+        E = tuplelist([])
+        superT = 0
+        for i in range(numofarc):
+            arci = grid[3+i].split()
+            E.append((arci[0],arci[1]))
+            G.add_edge(arci[0],arci[1])
+            superT = max(superT,int(arci[3]))
+        draw(G)
+        fullS = {}
+        fullF = tuplelist([])
+        for i in range(numoftask):
+            taski = grid[3+numofarc+i].split()
+            fullS[str(i)] = [superT,int(taski[2])]
+            route = nx.shortest_path(G,source=taski[0],target=taski[1])
+            for t in range(len(route)-1):
+                fullF.append((route[t],route[t+1],str(i)))
+        
 oldκ = tupledict({})
 for va,vb in E:
     oldκ[va,vb] = -1
@@ -148,14 +180,13 @@ while len(fullS) > tur*batch:
     for k in F:
         i = 0
         sizek = size[k[2]]
-        while sizek > 1500:
+        while sizek > 15:
             f[(k[0],k[1],k[2],i)] = [e2e[k[2]],15.0]
-            sizek -= 1500
+            sizek -= 15
             i += 1
-        f[(k[0],k[1],k[2],i)] = [e2e[k[2]],sizek/100]
+        f[(k[0],k[1],k[2],i)] = [e2e[k[2]],sizek]
     f,T,L =  multidict(f)
-    #f,T,L =  multidict({('ES1','BR1','s0','f0') : [125,15],('BR1','ES2','s0','f0') : [125,15]})
-    
+     
     md = Model('cyber')
     
     φ = md.addVars(f, name='phi')
@@ -209,9 +240,9 @@ while len(fullS) > tur*batch:
     for m in f:
         for n in f.select(m[1],'*',m[2],m[3]):
             md.addConstr((φ[m] + L[m] + δ <= φ[n]), 'Flow Transmission Constaints')
-        
     for s in S:
         fs = f.select('*','*',s,'*')
+        print(fs)
         md.addConstr((φ[fs[-1]] + L[fs[-1]] - φ[fs[0]] <= e2e[s]), 'End-to-End Constaints')
                                 
     for va,vb in E:
